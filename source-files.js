eb95a3f7a561cce7a6a76c3715a683490382ffa7
wilfredallyn <wilfredallyn@users.noreply.github.com>
2022-10-01 11:06:09 +0000

    deploy: 2e302d6ff96c8b34492deade0c3c1d96c26cccb7

--- source-files.js
+++ source-files.js
@@ -4,6 +4,7 @@ var sourcesIndex = JSON.parse('{\
 "configgen":["",[],["configgen.rs"]],\
 "fedimint_api":["",[["db",[],["batch.rs","mem_impl.rs","mod.rs"]],["encoding",[],["btc.rs","mod.rs","secp256k1.rs","tbs.rs"]],["module",[],["audit.rs","interconnect.rs","mod.rs","testing.rs"]]],["config.rs","lib.rs","macros.rs","rand.rs","task.rs","tiered.rs","tiered_multi.rs"]],\
 "fedimint_build":["",[],["lib.rs"]],\
+"fedimint_cli":["",[],["main.rs"]],\
 "fedimint_core":["",[],["config.rs","epoch.rs","lib.rs","outcome.rs","transaction.rs"]],\
 "fedimint_derive":["",[],["lib.rs"]],\
 "fedimint_ln":["",[["contracts",[],["account.rs","incoming.rs","mod.rs","outgoing.rs"]]],["config.rs","db.rs","lib.rs"]],\
@@ -15,7 +16,6 @@ var sourcesIndex = JSON.parse('{\
 "fedimintd":["",[],["main.rs"]],\
 "ln_gateway":["",[],["cln.rs","lib.rs","ln.rs","webserver.rs"]],\
 "mint_client":["",[["ln",[],["db.rs","incoming.rs","mod.rs","outgoing.rs"]],["mint",[],["db.rs","mod.rs"]],["wallet",[],["db.rs","mod.rs"]]],["api.rs","lib.rs","query.rs","transaction.rs","utils.rs"]],\
-"mint_client_cli":["",[],["main.rs"]],\
 "mint_rpc_client":["",[],["mint-rpc-client.rs"]],\
 "tbs":["",[["serde_impl",[],["mod.rs","scalar.rs"]]],["hash.rs","lib.rs","poly.rs"]]\
 }');
