6d25ec8780d37ff020454c3de962afa32d7d3444
wilfredallyn <wilfredallyn@users.noreply.github.com>
2022-10-17 10:22:57 +0000

    deploy: 0cf81c52305af181f787b07e704d2d5af35083eb

--- implementors/mint_client/trait.ModuleClient.js
+++ implementors/mint_client/trait.ModuleClient.js
@@ -1,5 +1,3 @@
 (function() {var implementors = {
-"fedimint_core_api":[],
-"fedimint_core_client":[],
-"fedimint_core_server":[]
+"mint_client":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
