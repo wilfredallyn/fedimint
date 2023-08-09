930998e7ec806bd3c0659b774e45908d4ef2c97b
wilfredallyn <wilfredallyn@users.noreply.github.com>
2023-08-09 21:42:38 +0000

    deploy: 05a03c8f02a31ad5c72c434e5087ac2c0fd98219

--- implementors/clap_builder/derive/trait.Args.js
+++ implementors/clap_builder/derive/trait.Args.js
@@ -1,11 +1,11 @@
 (function() {var implementors = {
-"dbtool":[["impl Args for <a class=\"struct\" href=\"dbtool/struct.Options.html\" title=\"struct dbtool::Options\">Options</a>"]],
 "faucet":[["impl Args for <a class=\"struct\" href=\"faucet/struct.Cmd.html\" title=\"struct faucet::Cmd\">Cmd</a>"]],
 "fedimint_cli":[["impl Args for <a class=\"struct\" href=\"fedimint_cli/struct.Opts.html\" title=\"struct fedimint_cli::Opts\">Opts</a>"]],
+"fedimint_dbtool":[["impl Args for <a class=\"struct\" href=\"fedimint_dbtool/struct.Options.html\" title=\"struct fedimint_dbtool::Options\">Options</a>"]],
 "fedimint_load_test_tool":[["impl Args for <a class=\"struct\" href=\"fedimint_load_test_tool/struct.Opts.html\" title=\"struct fedimint_load_test_tool::Opts\">Opts</a>"],["impl Args for <a class=\"struct\" href=\"fedimint_load_test_tool/struct.ConnectCommonArgs.html\" title=\"struct fedimint_load_test_tool::ConnectCommonArgs\">ConnectCommonArgs</a>"],["impl Args for <a class=\"struct\" href=\"fedimint_load_test_tool/struct.LoadTestArgs.html\" title=\"struct fedimint_load_test_tool::LoadTestArgs\">LoadTestArgs</a>"]],
 "fedimintd":[["impl Args for <a class=\"struct\" href=\"fedimintd/fedimintd/struct.ServerOpts.html\" title=\"struct fedimintd::fedimintd::ServerOpts\">ServerOpts</a>"]],
 "gateway_cli":[["impl Args for <a class=\"struct\" href=\"gateway_cli/struct.Cli.html\" title=\"struct gateway_cli::Cli\">Cli</a>"]],
 "gateway_cln_extension":[["impl Args for <a class=\"struct\" href=\"gateway_cln_extension/struct.ClnExtensionOpts.html\" title=\"struct gateway_cln_extension::ClnExtensionOpts\">ClnExtensionOpts</a>"]],
-"gatewayd":[["impl Args for <a class=\"struct\" href=\"gatewayd/struct.GatewayOpts.html\" title=\"struct gatewayd::GatewayOpts\">GatewayOpts</a>"]],
+"ln_gateway":[["impl Args for <a class=\"struct\" href=\"ln_gateway/struct.GatewayOpts.html\" title=\"struct ln_gateway::GatewayOpts\">GatewayOpts</a>"]],
 "recoverytool":[["impl Args for <a class=\"struct\" href=\"recoverytool/struct.RecoveryTool.html\" title=\"struct recoverytool::RecoveryTool\">RecoveryTool</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/clap_builder/derive/trait.Parser.js
+++ implementors/clap_builder/derive/trait.Parser.js
@@ -1,11 +1,11 @@
 (function() {var implementors = {
-"dbtool":[["impl Parser for <a class=\"struct\" href=\"dbtool/struct.Options.html\" title=\"struct dbtool::Options\">Options</a>"]],
 "faucet":[["impl Parser for <a class=\"struct\" href=\"faucet/struct.Cmd.html\" title=\"struct faucet::Cmd\">Cmd</a>"]],
 "fedimint_cli":[["impl Parser for <a class=\"struct\" href=\"fedimint_cli/struct.Opts.html\" title=\"struct fedimint_cli::Opts\">Opts</a>"]],
+"fedimint_dbtool":[["impl Parser for <a class=\"struct\" href=\"fedimint_dbtool/struct.Options.html\" title=\"struct fedimint_dbtool::Options\">Options</a>"]],
 "fedimint_load_test_tool":[["impl Parser for <a class=\"struct\" href=\"fedimint_load_test_tool/struct.Opts.html\" title=\"struct fedimint_load_test_tool::Opts\">Opts</a>"]],
 "fedimintd":[["impl Parser for <a class=\"struct\" href=\"fedimintd/fedimintd/struct.ServerOpts.html\" title=\"struct fedimintd::fedimintd::ServerOpts\">ServerOpts</a>"]],
 "gateway_cli":[["impl Parser for <a class=\"struct\" href=\"gateway_cli/struct.Cli.html\" title=\"struct gateway_cli::Cli\">Cli</a>"]],
 "gateway_cln_extension":[["impl Parser for <a class=\"struct\" href=\"gateway_cln_extension/struct.ClnExtensionOpts.html\" title=\"struct gateway_cln_extension::ClnExtensionOpts\">ClnExtensionOpts</a>"]],
-"gatewayd":[["impl Parser for <a class=\"struct\" href=\"gatewayd/struct.GatewayOpts.html\" title=\"struct gatewayd::GatewayOpts\">GatewayOpts</a>"]],
+"ln_gateway":[["impl Parser for <a class=\"struct\" href=\"ln_gateway/struct.GatewayOpts.html\" title=\"struct ln_gateway::GatewayOpts\">GatewayOpts</a>"]],
 "recoverytool":[["impl Parser for <a class=\"struct\" href=\"recoverytool/struct.RecoveryTool.html\" title=\"struct recoverytool::RecoveryTool\">RecoveryTool</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
